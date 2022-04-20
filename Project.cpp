63de4634e15ec57dc5acfec3d6126a9910700b38
Roman <92608037+Newtr@users.noreply.github.com>
2022-04-20 21:26:56 +0300

    Have issues with writing to file with

---
+++ Project.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+struct Student
+{
+    string Name;
+    float eng_mark, math_mark, phys_mark, sci_mark, art_mark;
+    double average_mark;
+    string status;
+
+    void GetInfo();
+    void Calculate();
+    void ShowData();
+};
+
+void Student::Calculate(){
+    average_mark = (eng_mark+math_mark+phys_mark+sci_mark+art_mark)/5;
+    if (average_mark>8)
+    {
+        status = "Best";
+    }
+    else if (average_mark>6)
+    {
+        status = "Good";
+    }
+    else if (average_mark>5)
+    {
+        status = "Bad";
+    }
+    else 
+    {
+        status = "Awful";
+    }
+}
+
+void Student::GetInfo(){
+    cout<<"\nStudent name: ";
+    cin>>Name;
+    cout<<"\nEnter marks in English: ";
+    cin>>eng_mark;
+    cout<<"\nEnter marks in Math: ";
+    cin>>math_mark;
+    cout<<"\nEnter marks in Physics: ";
+    cin>>phys_mark;
+    cout<<"\nEnter marks in Science: ";
+    cin>>sci_mark;
+    cout<<"\nEnter marks in Art: ";
+    cin>>art_mark;
+    Calculate();
+}
+
+void Student::ShowData(){
+    cout<<"Student name: "<<Name;
+    cout<<"English: "<<eng_mark;
+    cout<<"Math: "<<math_mark;
+    cout<<"Physics: "<<phys_mark;
+    cout<<"Science: "<<sci_mark;
+    cout<<"Art: "<<art_mark;
+    cout<<"Status of student: "<<status;
+}
+
+void CreateStudent()
+{
+    Student stud;
+    string str;
+    ofstream OFile;
+    OFile.open("Student.txt", ios::binary|ios::app);
+    stud.GetInfo();
+/*     OFile<<stud.Name<<endl;
+    OFile<<stud.eng_mark<<endl;
+    OFile<<stud.math_mark<<endl;
+    OFile<<stud.phys_mark<<endl;
+    OFile<<stud.sci_mark<<endl;
+    OFile<<stud.art_mark<<endl;
+    OFile<<stud.status<<endl; */
+    OFile.write(reinterpret_cast<char*>(&stud), sizeof(stud));
+    OFile.close();
+    cout<<"Student was created successfully!"<<endl;
+}
+void ShowAllInfo()
+{
+    string str;
+    Student stud;
+    ifstream IFile;
+    IFile.open("Student.txt", ios::binary);
+    if (!IFile)
+    {
+        cout<<"ERROR! Press any key to exit ...";
+        cin.ignore();
+        cin.get();
+        return;
+    }
+    cout<<"INFORMATION\n\n";
+    while (!IFile.eof())
+    {
+        getline(IFile,str);
+        cout<<str<<endl;
+        if (str=="")
+        {
+            break;
+        }
+        
+    }
+    
+    IFile.close();
+    cin.ignore();
+    cin.get();
+}
+/* void DeleteStudent(int); */
+
+int main(){
+    int UserChoose;
+    do{
+    cout<<"1)Create Student"<<endl;
+    cout<<"2)Show All Information about students"<<endl;
+/*     cout<<"3)Delete Student"<<endl; */
+    cout<<"4)Exit"<<endl;
+    cout<<"Your choose : "; cin>>UserChoose;
+    switch (UserChoose)
+    {
+    case 1:
+        CreateStudent();
+        break;
+    case 2:
+        ShowAllInfo();
+        break;
+    }
+    }while(UserChoose!=4);
+    return 0;
+}
